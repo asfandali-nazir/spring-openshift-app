6f8d372e7ec78acf1f352ee09cc94c5ec2cd3551
asfandali-nazir <155414776+asfandali-nazir@users.noreply.github.com>
2024-01-03 10:35:52 +0500

    Update MiscController.java

--- src/main/java/com/sl/openshiftdemo/controller/MiscController.java
+++ src/main/java/com/sl/openshiftdemo/controller/MiscController.java
@@ -1,5 +1,6 @@
 package com.sl.openshiftdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,21 +10,25 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class MiscController {
   WebClient client = WebClient.create("http://172.30.143.246:8080");
 
   @GetMapping
   public String sayHello() {
+    log.info("sayHello() :: started");
     return "Hello from Spring server";
   }
 
   @GetMapping("/input")
   public String sayHelloWithInput(@RequestParam String input) {
+    log.info("sayHelloWithInput() :: started");
     return "Hello! how are you doing " + input + "?";
   }
 
   @GetMapping("/get-data-from-pod")
   public String sayHelloFromPod() {
+    log.info("sayHelloFromPod() :: started");
     String response =
         client.get().retrieve().bodyToMono(String.class).onErrorResume(e -> Mono.empty()).block();
     System.out.println(response);
